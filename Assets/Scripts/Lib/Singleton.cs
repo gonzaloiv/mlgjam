5130833ce070e7eed7364aa4ffa96c3b5198b2ea
gonzaloiv <aelthalion@gmail.com>
2018-07-05 18:26:09 +0200

    Imports RSG.Promise and DOTween, adds ConfigSystem

---
+++ Assets/Scripts/Lib/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    #region Fields
+
+    protected static T instance;
+
+    #endregion
+
+    #region  Singleton Pattern
+
+    public static T Instance {
+        get {
+            if (instance == null)
+                instance = (T) FindObjectOfType(typeof(T));
+            return instance;
+        }
+    }
+
+    #endregion
+
+    #region Public Behaviour
+
+    public static void Delete () {
+        Debug.Log("Deleting Singleton of type: " + typeof(T));
+        instance = default(T);
+    }
+
+    #endregion
+
+}
